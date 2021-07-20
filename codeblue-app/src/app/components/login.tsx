58db4c18eed5dbe8a50bcfe5bfc23138acbf4c1c
Cristina <cristoshiwassaki@gmail.com>
2021-07-19 23:27:15 -0300

    melhora no alinhamento

--- codeblue-app/src/app/components/login.tsx
+++ codeblue-app/src/app/components/login.tsx
@@ -1,68 +1,78 @@
 import React from "react";
-import { VStack, HStack, Input, Flex } from "@chakra-ui/react";
-import { Icon, Button, Text } from "@chakra-ui/react";
+import { VStack, HStack, Input } from "@chakra-ui/react";
+import { Icon, Button, Text, Link } from "@chakra-ui/react";
 import { InputGroup, InputLeftElement } from '@chakra-ui/react';
 import { MdEmail } from 'react-icons/md';
 import { RiLockPasswordFill } from 'react-icons/ri'
 import { BsBoxArrowInRight } from 'react-icons/bs'
 import ImText from './capa'
 
-export default function Login(){
-  return(
-    <HStack padding='8' bg='brand.300' direction='row' justifyContent='center'>
-        <VStack margin-top='20'>
-          <Flex color='white'>
-            Faça seu login
-          </Flex>
-            <InputGroup>
-            <InputLeftElement
-              color='gray.200'
-              pointerEvents="none"
-              boxSize={12}
-              children={<MdEmail/>}
-            />
-          <Input 
+export default function Login() {
+  return (
+    <HStack padding='12' bg='brand.300' direction='row' justifyContent='center'>
+      <VStack spacing={2}>
+        <Text color='white'>
+          Faça seu login
+        </Text>
+        <InputGroup>
+          <InputLeftElement
+            color='gray.200'
+            pointerEvents="none"
+            boxSize={12}
+            children={<MdEmail />}
+          />
+          <Input
             type="email"
-            variant='filled' 
-            placeholder="E-mail"
+            variant='filled'
+            placeholder='Senha'
             width='340px'
-            height='56px' 
+            height='56px'
             borderRadius='10px' />
-          </InputGroup>
-          <InputGroup>
-            <InputLeftElement
-              color='gray.200' 
-              pointerEvents="none"
-              boxSize={12}
-              children={<RiLockPasswordFill/>}
-            />
-            <Input 
-              type="password"
-              variant='filled' 
-              placeholder="Senha"
-              width='340px'
-              height='56px' 
-              borderRadius='10px' />
-            </InputGroup>
-          <Button 
-            type="button"
+        </InputGroup>
+
+        <InputGroup>
+          <InputLeftElement
+            color='gray.200'
+            pointerEvents="none"
+            boxSize={12}
+            children={<RiLockPasswordFill />}
+          />
+          <Input
+            type="password"
+            variant='filled'
+            placeholder='Senha'
             width='340px'
-            height='56px' 
-            borderRadius='10px' 
-          >
-              Entrar
-          </Button>
-          <Text fontSize='16px' color='white'>
+            height='56px'
+            borderRadius='10px' />
+        </InputGroup>
+        <Button
+          type="button"
+          width='340px'
+          height='38px'
+          borderRadius='10px'
+        >
+          Entrar
+        </Button>
+
+        <Link href='#'>
+          <Text fontSize='15px' color='white'>
             Esqueci minha senha
           </Text>
-          <Text fontSize='14px' color='white'>
-            <Icon as={BsBoxArrowInRight} color='brand.900'/>
+        </Link>
+
+        <Link href='#'>
+          <Text
+            fontSize='14px'
+            color='white'
+            mt='25px'>
+            <Icon as={BsBoxArrowInRight} color='brand.900' />
             Criar Conta
           </Text>
-        </VStack>
-        
-        <ImText />
-          
-      </HStack>
+        </Link>
+
+      </VStack>
+
+      <ImText />
+    </HStack>
   )
 }
--- codeblue-app/src/app/components/logon.tsx
+++ codeblue-app/src/app/components/logon.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { VStack, HStack, Input, Flex } from "@chakra-ui/react";
-import { Icon, Button, Text } from "@chakra-ui/react";
+import { VStack, HStack, Input } from "@chakra-ui/react";
+import { Icon, Button, Text, Link } from "@chakra-ui/react";
 import { InputGroup, InputLeftElement } from '@chakra-ui/react';
 import { MdEmail } from 'react-icons/md';
 import { RiLockPasswordFill } from 'react-icons/ri'
@@ -8,77 +8,79 @@ import { BsBoxArrowInRight } from 'react-icons/bs'
 import { FaUser } from 'react-icons/fa'
 import ImText from './capa'
 
-export default function Logon(){
-  return(
-    <HStack padding='8' bg='brand.300' direction='row' justifyContent='center'>
-        <VStack margin-top='20' spacing={3}>
-          <Flex color='white'>
-            Faça seu cadastro
-          </Flex>
+export default function Logon() {
+  return (
+    <HStack padding='12' bg='brand.300' direction='row' justifyContent='center'>
+      <VStack margin-top='20' spacing={2}>
+        <Text color='white'>
+          Faça seu cadastro
+        </Text>
 
-          <InputGroup>
-            <InputLeftElement
-              color='gray.200'
-              pointerEvents="none"
-              boxSize={12}
-              children={<FaUser />}
-            />
-          <Input 
+        <InputGroup>
+          <InputLeftElement
+            color='gray.200'
+            pointerEvents="none"
+            boxSize={12}
+            children={<FaUser />}
+          />
+          <Input
             type="email"
-            variant='filled' 
+            variant='filled'
             placeholder="Username"
             width='340px'
-            height='56px' 
+            height='56px'
             borderRadius='10px' />
-          </InputGroup>
+        </InputGroup>
 
-            <InputGroup>
-            <InputLeftElement
-              color='gray.200'
-              pointerEvents="none"
-              boxSize={12}
-              children={<MdEmail/>}
-            />
-          <Input 
+        <InputGroup>
+          <InputLeftElement
+            color='gray.200'
+            pointerEvents="none"
+            boxSize={12}
+            children={<MdEmail />}
+          />
+          <Input
             type="email"
-            variant='filled' 
+            variant='filled'
             placeholder="E-mail"
             width='340px'
-            height='56px' 
+            height='56px'
             borderRadius='10px' />
-          </InputGroup>
-          <InputGroup>
-            <InputLeftElement
-              color='gray.200' 
-              pointerEvents="none"
-              boxSize={12}
-              children={<RiLockPasswordFill/>}
-            />
-            <Input 
-              type="password"
-              variant='filled' 
-              placeholder="Senha"
-              width='340px'
-              height='56px' 
-              borderRadius='10px' />
-            </InputGroup>
-          <Button 
-            type="button"
+        </InputGroup>
+        <InputGroup>
+          <InputLeftElement
+            color='gray.200'
+            pointerEvents="none"
+            boxSize={12}
+            children={<RiLockPasswordFill />}
+          />
+          <Input
+            type="password"
+            variant='filled'
+            placeholder="Senha"
             width='340px'
-            height='56px' 
-            borderRadius='10px' 
-          >
-              Criar Conta
-          </Button>
+            height='56px'
+            borderRadius='10px' />
+        </InputGroup>
+        <Button
+          type="button"
+          width='340px'
+          height='38px'
+          borderRadius='10px'
+        >
+          Criar Conta
+        </Button>
 
+        <Link>
           <Text fontSize='14px' color='white'>
             <Icon as={BsBoxArrowInRight} color='brand.900' />
             Entrar com a minha conta
           </Text>
-        </VStack>
-        
-        <ImText />
-          
-      </HStack>
+        </Link>
+      </VStack>
+
+      <ImText />
+
+    </HStack>
   )
 }
