7d9d0def29c5b44534afd84c29c9e89c6caea81f
Cristina <cristoshiwassaki@gmail.com>
2021-08-03 16:06:04 -0300

    criada tela de progresso

--- codeblue/pages/progress.tsx
+++ codeblue/pages/progress.tsx
@@ -1,10 +1,23 @@
 import React from "react";
 import { Layout } from "../components/Layout";
+import { Text, Box } from '@chakra-ui/react'
+import LanguageProgress  from '../components/LanguageProgress'
 
 const progress = () => {
   return (
     <Layout title="progresso" currentPath="progress">
-      {" "}
+      <Text
+        color='gray.100'
+      >
+        Exercite seu cérebro e aumente suas conexões
+      </Text>
+      <Box bg='gray.50' p={3} mt={9} borderRadius='10'>
+        <LanguageProgress language='c#' value={50} />
+        <LanguageProgress language='javascript' value={85} />
+        <LanguageProgress language='sql' value={30} />
+        <LanguageProgress language='python' value={50} />
+      </Box>
+
     </Layout>
   );
 };
